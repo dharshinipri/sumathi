7a9c0013954a3d8e7a492a83e15daabdbaba7dce
dharshinipri <48049095+dharshinipri@users.noreply.github.com>
2019-02-27 14:39:14 +0530

    Create positivenegative.java

---
+++ positivenegative.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Demo
+{
+    public static void main(String[] args)
+{
+    int i=3;
+    if(i<0)
+    {
+        System.out.println("Negative");
+    }
+    else if(i>0)
+    {
+        System.out.println("Positive");
+    }
+    else
+    {
+        System.out.println("Zero");
+    }
+}
+}
